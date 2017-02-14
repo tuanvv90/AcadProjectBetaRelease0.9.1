553af3530be510b5643f1d53f5986ce2519cb0d3
Tuan Vu <tuanvvk53.hust@gmail.com>
2017-02-14 21:40:41 +0700

    Add version manager.
    Add version manager.

---
+++ AcadProject/VersionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcadProjectVersionManager
+{
+    class VersionManager
+    {
+        //Beta version release 2nd
+        private String RELEASE_VERSION = "0.9.1";
+
+        public VersionManager() { }
+        public String getReleaseVersion()
+        {
+            return this.RELEASE_VERSION;
+        }
+    }
+}
